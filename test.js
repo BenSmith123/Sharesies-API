9ba2071fe58d8101bec7cfabae1180c0dc0d7976
benjiiman12@gmail.com <benjiiman12@gmail.com>
2020-10-10 21:45:53 +1300

    vscode & npm test commands

---
+++ test.js
@@ -0,0 +1,24 @@
+
+require('dotenv').config();
+
+const sharesies = require('./index');
+// const sharesies = require('sharesies'); <-- for published npm package
+
+(async () => {
+
+	console.log('Authenticating..');
+
+	const credentials = {
+		email: process.env.EMAIL,
+		password: process.env.PASSWORD
+	};
+
+	const auth = await sharesies.authenticate(credentials);
+
+	console.log(auth);
+
+	// const a = await sharesies.check(cookie);
+	// console.log(a);
+
+
+})();
