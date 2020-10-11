83b69bffdb3ec74e0974bc657e841e1a58f171f0
benjiiman12@gmail.com <benjiiman12@gmail.com>
2020-10-11 23:00:46 +1300

    working API get requests

--- src/index.js
+++ src/index.js
@@ -1,24 +1,43 @@
 
 const axios = require('axios');
 
-const { ENDPOINTS } = require('./config');
+const URL_BASE = 'https://app.sharesies.nz/api';
+
+const ENDPOINTS = {
+	LOGIN: `${URL_BASE}/identity/login`,
+	LOGOUT: `${URL_BASE}/identity/logout`,
+	CHECK: `${URL_BASE}/identity/check`,
+	STATS: `${URL_BASE}/accounting/stats-v3`,
+	INFO: `${URL_BASE}/instruments/info`,
+	FUND_LIST: `${URL_BASE}/fund/list`,
+	TRANS_HIST: `${URL_BASE}/accounting/transaction-history`,
+	FUND_HIST: `${URL_BASE}/fund/price-history?first=0001-01-01`
+};
 
 
+// let cookie = null;
 let cookie = null;
-// let user = null;
-const userId = null;
+let user = null;
+let userId = null;
 
 
-function checkCookie() {
-	if (!cookie) { throw new Error('No cookie set, you must authenticate first'); }
+/**
+ * Returns the currently logged in user, or null if user has not yet logged in
+ *
+ * @returns {object}
+ */
+function getUser() {
+	return user;
 }
 
 
 /**
- * Returns the authentication cookie
+ * Returns true once user has been signed in
  *
- * @param {string} email
- * @param {string} password
+ * @param {object} auth
+ * @param {string} auth.email
+ * @param {string} auth.password
+ * @returns {Promise<Boolean>}
  */
 async function authenticate({ email, password }) {
 
@@ -33,51 +52,49 @@ async function authenticate({ email, password }) {
 		}
 	};
 
-	try {
-		const res = await axios(req);
+	const res = await axios(req);
 
-		// TODO - validate successful login
+	// TODO - validate successful login
+	if (!res.headers['set-cookie'][1] || !res.data.user) { throw new Error('Failed to login: Auth cookie or user data was not returned'); }
 
-		cookie = res.headers['set-cookie'][1]; // TODO - why this header (in browser it looks like a different value is sent)
-		// user = res.data.user; // TODO - need to store this?
-		// userId = res.data.user.id;
+	cookie = res.headers['set-cookie'][1]; // TODO - why this header (in browser it looks like a different value is sent)
+	user = res.data.user; // TODO - need to store this?
+	userId = res.data.user.id;
 
-		return res;
-
-
-	} catch (err) {
-		// TODO
-		console.error(err);
-
-	}
-
+	return { cookie, user };
 }
 
 
-async function check() {
+/**
+ * Returns the [GET] sharesies API response
+ *
+ * @param {string} endpoint
+ * @param {string=} queryParams
+ */
+async function sharesiesAPI(endpoint, queryParams = '') {
 
-	checkCookie();
+	if (!cookie) { throw new Error('No cookie set, you must authenticate first'); }
 
 	const req = {
-		url: ENDPOINTS.CHECK,
-		headers: {
-			cookie
-		}
+		url: queryParams
+			? endpoint + queryParams + userId // attach userId to any API request that has query params
+			: endpoint,
+		headers: { cookie }
 	};
 
-	try {
-		const res = await axios(req);
+	const res = await axios(req);
 
-		console.log(res);
-
-		return res;
-	} catch (err) {
-		console.error(err);
-	}
+	return res.data;
 }
 
 
 module.exports = {
 	authenticate,
-	check
+	check: () => sharesiesAPI(ENDPOINTS.CHECK),
+	stats: () => sharesiesAPI(ENDPOINTS.STATS, '?acting_as_id='),
+	info: () => sharesiesAPI(ENDPOINTS.INFO),
+	fundList: () => sharesiesAPI(ENDPOINTS.FUND_LIST, '?limit=50&acting_as_id='),
+	transHist: () => sharesiesAPI(ENDPOINTS.TRANS_HIST),
+	fundHist: () => sharesiesAPI(ENDPOINTS.FUND_HIST),
+	cookie // TODO - remove from exports (only exported as debug to avoid spamming sharesies login)
 };
--- test.js
+++ test.js
@@ -1,24 +1,30 @@
 
 require('dotenv').config();
+const fs = require('fs');
+const { cookie } = require('./authResponse.json'); // debug
 
 const sharesies = require('./src/index');
 // const sharesies = require('sharesies'); <-- for published npm package
 
 (async () => {
 
-	console.log('Authenticating..');
+	console.log('Starting..');
 
 	const credentials = {
 		email: process.env.EMAIL,
 		password: process.env.PASSWORD
 	};
 
-	const auth = await sharesies.authenticate(credentials);
+	/// /// ///
+	// TEMP skip logging in when you can to avoid spamming sharesies API
+	// const authResponse = await sharesies.authenticate(credentials);
 
-	console.log(auth);
+	// debug - store the last auth details to avoid spamming sharesies login
+	// fs.writeFileSync('authResponse.json', JSON.stringify(authResponse));
+	/// /// ///
 
-	// const a = await sharesies.check(cookie);
-	// console.log(a);
+	const data = await sharesies.fundList();
 
+	console.log(data);
 
 })();
